96c714868bfc75f0f67f86a6af5d1d87c4f144b1
sealand0508 <37231425+sealand0508@users.noreply.github.com>
2018-03-10 14:14:13 +0800

    修改第一次
    增加 第二個函數

--- Utils.java
+++ Utils.java
@@ -7,6 +7,10 @@ class Utils{
   }
   public static String getDescript (){
     return "作者:"+AUTHOR + "/n"+ "公司:"+COMPANY";
-
   }
+  public String[] getFunctionList(){
+    String returnValue[];
+    returnValue = new returnValue[30];
+    return returnValue;
+  } 
 }
