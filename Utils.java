d4f3675511842d3f2ddb6d7ca35b7b42756817cf
sealand0508 <37231425+sealand0508@users.noreply.github.com>
2018-03-10 14:00:02 +0800

    Create Utils.java

---
+++ Utils.java
@@ -0,0 +1,12 @@
+class Utils{
+  static final String COMPANY="慈濟醫療財團法人";  
+  static final String AUTHOR="陳德順";
+  
+  public Utils(){
+    
+  }
+  public static String getDescript (){
+    return "作者:"+AUTHOR + "/n"+ "公司:"+COMPANY";
+
+  }
+}
